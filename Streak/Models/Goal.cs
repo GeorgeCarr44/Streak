95d28371d6e5e2d3b481d37ef87cda5b6cb7ab9f
George Carr <gwcgreen3@gmail.com>
2023-12-31 10:57:03 +0000

    Renames and app structure

---
+++ Streak/Models/Goal.cs
@@ -0,0 +1,24 @@
+﻿using HealthKit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streak.Models
+{
+    public class Goal
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required DateTime CreationDate { get; set; }
+        public string? Description { get; set; }
+        public int CurrentStreak { get; set; }
+        public int LongestStreak { get; set; }
+
+        public Goal() { }
+    }
+}
